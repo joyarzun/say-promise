1677376a5b72b7417e98d63dafa94c8131518ce8
Jonnatan Oyarzun <jonsxaero@gmail.com>
2017-08-06 10:43:25 -0400

    Fix stop

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const promise = {
   speak: (text, voice, speed) => pify(say.speak)(text, voice, speed),
   export: (text, voice, speed, file) =>
     pify(say.export)(text, voice, speed, file),
-  stop: pify(say.stop)()
+  stop: pify(say.stop)
 };
 
 module.exports = promise;
